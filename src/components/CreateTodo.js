9bf77d07f6b0edb88ce6704b53cb85ff7f055b95
antonkarpach <karpach.anton@gmail.com>
2019-10-02 23:19:44 +0300

    first commit

---
+++ src/components/CreateTodo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CrateTodo extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome tot Create Todo Component!!!</p>
+            </div>
+        );
+    }
+}
